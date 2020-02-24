0e477dbd43ea145515f9ce1197cd680ccdeebf53
Rolf Suter <rolf.suter@gmx.net>
2020-02-24 20:48:19 +0100

    Add basic Tree datastructure and processing functions

---
+++ MailBoxTransformer.fsx
@@ -0,0 +1,40 @@
+
+// Tree datastructure and processing functions
+// Reference: https://fsharpforfunandprofit.com/posts/recursive-types-and-folds-3b
+
+type Tree<'LeafData,'INodeData> =
+    | LeafNode of 'LeafData
+    | InternalNode of 'INodeData * Tree<'LeafData,'INodeData> seq
+
+// cata is bottom-up recursion
+let rec cata fLeaf fNode (tree:Tree<'LeafData,'INodeData>) :'r = 
+    let recurse = cata fLeaf fNode  
+    match tree with
+    | LeafNode leafInfo -> 
+        fLeaf leafInfo 
+    | InternalNode (nodeInfo,subtrees) -> 
+        fNode nodeInfo (subtrees |> Seq.map recurse)
+
+// fold is top-down iteration    
+let rec fold fLeaf fNode acc (tree:Tree<'LeafData,'INodeData>) :'r = 
+    let recurse = fold fLeaf fNode  
+    match tree with
+    | LeafNode leafInfo -> 
+        fLeaf acc leafInfo 
+    | InternalNode (nodeInfo,subtrees) -> 
+        // determine the local accumulator at this level
+        let localAccum = fNode acc nodeInfo
+        // thread the local accumulator through all the subitems using Seq.fold
+        let finalAccum = subtrees |> Seq.fold recurse localAccum 
+        // ... and return it
+        finalAccum 
+
+
+
+type MailBoxInfo = 
+    { Name: string }
+
+type FolderInfo = 
+    { Name: string }
+
+type MailBoxItem = Tree<MailBoxInfo, FolderInfo>
